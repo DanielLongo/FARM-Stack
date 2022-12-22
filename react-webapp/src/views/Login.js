4a33d93a80592167b94972e39c143bf0f25af9d6
Ariane Lee <84607438+arianexlee@users.noreply.github.com>
2022-12-21 23:09:12 -0500

    Pushing PR?? (#3)
    * updated footer
    
    * beautified landing page, login, and signup

--- react-webapp/src/views/Login.js
+++ react-webapp/src/views/Login.js
@@ -14,7 +14,7 @@ function LoginForm({onClose, showSignUp, showPasswordReset, showConfirm, setUser
 
   const navigation = useNavigate();
 
-  const hanldeLogin = async () => {
+  const handleLogin = async () => {
     setUsername(email)
     const response = await login(email, password)
     if (response === 'success') {
@@ -32,43 +32,47 @@ function LoginForm({onClose, showSignUp, showPasswordReset, showConfirm, setUser
     <Modal onClose={onClose}>
         <div
         className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 w-6/12 md:w-4/12">
-            <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <div className="px-4 py-4 sm:px-6 sm:flex sm:flex-row-reverse">
             <button onClick={onClose} className="p-2 rounded-lg bg-white hover:bg-slate-100">
             <XIcon className="w-5 text-gray h-5 font-bold " aria-hidden="true"/>
             </button>
             </div>
-            <div className='flex flex-row items-center justify-around mb-12'>
+            <div className='flex flex-row items-center justify-around mb-16'>
                 <div className='px-12'>
                     <h1 className="text-2xl text-center font-bold text-left mb-8">Login</h1>
                     <button onClick={googleAuth} className="px-4 py-2 bg-slate-100 focus:bg-white rounded-lg w-full focus:border-slate-500 flex-row justify-around items-center flex">
-                    <div className="flex flex-row">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google logo" className="w-6 h-6 mr-2" />
-                        <p className="font-semibold">Sign In with Google</p>
+                    <div className="flex flex-row items-center">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google logo" className="w-4 h-4 mr-2" />
+                        <p className="font-semibold text-slate-500">Login with Google</p>
                     </div>
                     </button>
-                    
-                    <hr class="mx-auto my-6 w-48 h-1 bg-gray-100 rounded border-0 dark:bg-gray-700"/>
+                    <hr class="mt-8 mb-6 h-0.5 bg-gray-100 rounded border-0 dark:bg-gray-100"/>
+                        <label className='text-xs font-semibold' for="email">Email</label> 
                         <input
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="mb-4 px-4 py-2 bg-slate-100 focus:bg-white rounded-lg w-full focus:border-slate-500"
+                            className="mt-1 mb-4 px-4 py-2 placeholder-slate-500 bg-slate-100 focus:bg-white rounded-lg w-full focus:border-slate-500"
                             id="email"
                             type="email"
                             placeholder="Enter your email"
                         />
+                        <label className='text-xs font-semibold' for="password">Password</label> 
                         <input
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            className="px-4 py-2 bg-slate-100 focus:bg-white rounded-lg w-full focus:border-slate-500"
+                            className="mt-1 px-4 py-2 placeholder-slate-500 bg-slate-200 focus:bg-white rounded-lg w-full focus:border-slate-500"
                             id="password"
                             type="password"
                             placeholder="Enter your password"
                         />
-                        <p className="text-xs mt-4">Forgot Your Password? <a onClick={showPasswordReset} className='text-blue-800 cursor-pointer'>Reset Password</a></p>
-                        <p className="text-xs mb-2">Don't have an account? <a onClick={showSignUp} className='text-blue-800 cursor-pointer'>Sign Up</a></p>
-                        <button onClick={hanldeLogin} className="p-2 mb-4 w-full font-semibold bg-transparent border-[1px] border-black rounded-lg hover:bg-gray-200 hover:text-gray-700 mt-4">
+                            <p className="text-xs mt-4 mb-1"><a onClick={showPasswordReset} className='hover:underline text-blue-800 cursor-pointer'>Reset Password</a></p>
+                        <button onClick={handleLogin} className="p-2 mb-4 w-full text-white font-semibold bg-blue-800 rounded-lg hover:bg-blue-300 hover:text-gray-800 mt-4">
                             Login
                         </button>
+                        <div className= 'flex flex-row justify-center'>
+                        <p className="text-xs mb-2">Don't have an account? <a onClick={showSignUp} className='text-blue-800 cursor-pointer hover:underline'>Sign Up</a></p>
+                        </div>
+
                 </div>
             </div>
         </div>
