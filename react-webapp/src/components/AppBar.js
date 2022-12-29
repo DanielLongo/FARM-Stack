507e2f0134d457f6922bf0169967961a7d35112e
Daniel Longo <danieldominiclongo@gmail.com>
2022-12-29 00:21:27 -0800

    circle ci (#22)
    * Production (#9)
    
    * github actions for prod (#7)
    
    * Update main.yml
    
    * Update README.md
    
    * Production (#10)
    
    * github actions for prod (#7)
    
    * Update main.yml
    
    * Update README.md
    
    * Production (#11)
    
    * github actions for prod (#7)
    
    * Update main.yml
    
    * Update README.md
    
    * github actions
    
    * github actions
    
    * github actions
    
    * deploy to ecr
    
    * deploy to ecr 2
    
    * deploy to ecr 2 on push
    
    * deploy to ecr 3 on push
    
    * deploy to ecr 4 on push
    
    * deploy to ecr with iam role on push
    
    * deploy to ecr permissions
    
    * landing page and notifications added
    
    * todos
    
    * circle
    
    * circle to yml

--- react-webapp/src/components/AppBar.js
+++ react-webapp/src/components/AppBar.js
@@ -28,14 +28,17 @@ function AppBar(props) {
   return (
     <>
       {authModalType && <AuthModal setAuthModalType={setAuthModalType} type={authModalType} />}
-      <div className="backdrop-blur-xl bg-slate-0/[.65] bg-red p-4 flex justify-between items-center fixed top-0 left-0 right-0 z-9 md:mx-16 lg:mx-32">
+      <div className="backdrop-blur-xl bg-slate-0/[.65] flex justify-between items-center fixed top-0 left-0 right-0 z-10">
+        <div className="md:mx-16 lg:mx-32 flex w-full p-4 justify-between">
         <a href="/"><h1 className="text-xl text-black">[App Name]</h1></a>
         <div>
           {
             JSON.parse(localStorage.getItem("isAuthed")) ? (
               <div>
                 <div className="flex flex-row">
-                  <Button onClick={signOutWrapper} text={"Sign out"} size={"md"} disabled={false} type="primary" />
+                  <button className="btn-secondary" onClick={signOutWrapper}>Sign Out</button>
+                  {window.location.pathname === "/" && <button className="btn-primary ml-2" onClick={() => navigate('/home')}>To Dashboard</button>}
+                  
                 </div>
               </div>
             ) : (
@@ -48,6 +51,7 @@ function AppBar(props) {
               </div>
             )
           }
+          </div>
 
         </div >
       </div >
